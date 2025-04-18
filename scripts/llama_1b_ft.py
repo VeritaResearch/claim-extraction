b826728cdce8fe7e3f2bce6b3c5400b57c300891
Andrew Bell <abell5@g.clemson.edu>
2025-04-18 18:28:59 -0400

    Add llama_1b_ft script

---
+++ scripts/llama_1b_ft.py
@@ -0,0 +1,65 @@
+import os
+from os.path import join, dirname, pardir
+
+import sys
+
+PROJ_PATH = join(dirname(__file__), pardir)
+sys.path.insert(0, PROJ_PATH)
+
+import pandas as pd
+from datasets import Dataset
+from trl import SFTConfig, SFTTrainer
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from src.prompts import FactcheckGPT_SYSTEM_PROPMT, CHECKWORTHY_PROMPT, SPECIFY_CHECKWORTHY_CATEGORY_PROMPT
+
+cuda = True
+cache_dir = "../assets/pretrained-models"
+model_path = "meta-llama/Llama-3.2-1B-Instruct"
+output_dir="../assets/finetuned-models/Llama-3.2-1B-Instruct-SFT"
+full_data_path = '../data/ours/train.csv'
+
+def prepare_dataset():
+    dataset = pd.read_csv(full_data_path)
+    dataset = dataset.dropna()
+
+    dataset["label_yes_no"] = dataset["label"].apply(
+        lambda x: "Yes" if x == 1 else "No"
+    )
+    dataset["messages"] = dataset.apply(
+        lambda row: [
+            {"role": "system", "content": FactcheckGPT_SYSTEM_PROPMT},
+            {"role": "user", "content": CHECKWORTHY_PROMPT.format(texts = ('["' + row['text'] + '"]'))}
+        ],
+        axis=1,
+    )
+    dataset = dataset.filter(items=["messages"])
+
+    train_dataset = Dataset.from_pandas(dataset)
+    return train_dataset
+
+training_args = SFTConfig(
+    output_dir=output_dir,
+    logging_steps=10,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=16,
+    learning_rate=5e-5,
+    num_train_epochs=3,
+    bf16=True,
+    ) # bfloat16 training 
+
+if __name__ == "__main__":
+    model = AutoModelForCausalLM.from_pretrained(
+        model_path, cache_dir=cache_dir, use_safetensors=True
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_path, cache_dir=cache_dir, use_safetensors=True
+    )
+
+    if cuda:
+        model.to("cuda")
+
+    train_dataset = prepare_dataset()
+
+    trainer = SFTTrainer(model=model, args=training_args, train_dataset=train_dataset)
+    trainer.train()
+    trainer.save_model()
