6ec8454e03b2d6ec901b3fca36f32329cb847004
joaopfonseca <jpfonseca@novaims.unl.pt>
2025-05-07 11:29:48 -0400

    ENH update claim sourcing

--- src/claim_sourcer/_sourcer.py
+++ src/claim_sourcer/_sourcer.py
@@ -1,49 +1,79 @@
+import re
+from tqdm.auto import tqdm
 import requests
+import time
 import tempfile
+import json
 from urllib.parse import urlparse, urljoin
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
+from trafilatura import extract, fetch_url
 from bs4 import BeautifulSoup
 from atlas.document_processing import split_sentences, pdf_to_text
 
-SOURCES = {"congressional record": "https://www.congress.gov/congressional-record"}
+SOURCES = {"congressional record": "https://api.congress.gov/v3/"}
 
 
-def get_congressional_statements():
+def get_congressional_statements(api_key: str) -> list:
     """
     Fetches the most recent congressional record from the official website.
     """
     url = SOURCES["congressional record"]
 
-    # Set up Selenium WebDriver
-    options = Options()
-    options.add_argument("--headless")  # Run in headless mode
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Firefox(options=options)
+    records = requests.get(
+        urljoin(url, "daily-congressional-record"), params={"api_key": api_key}
+    ).content
+    records = json.loads(records)["dailyCongressionalRecord"]
 
-    # Load the page
-    driver.get(url)
+    ignore_titles = [
+        "PLEDGE OF ALLEGIANCE;",
+        "PRAYER;",
+        "ADJOURNMENT",
+        "ADDITIONAL SPONSORS",
+    ]
+    all_articles = []
+    for record in records:
+        record_url = urljoin(record["url"].split("?")[0] + "/", "articles")
+        record = None
+        while record is None:
+            record = requests.get(
+                record_url,
+                params={
+                    "api_key": api_key,
+                    "offset": 0,
+                    "limit": 250,
+                },
+            ).content
+            if record is None:
+                print("Error fetching record, retrying...")
+                time.sleep(5)
+        record = json.loads(record)["articles"]
+        articles = [
+            {
+                "url": {source["type"]: source["url"] for source in article["text"]}[
+                    "Formatted Text"
+                ],
+                "title": article["title"],
+            }
+            for section in record
+            for article in section["sectionArticles"]
+        ]
+        articles = [
+            article
+            for article in articles
+            if not any(article["title"].startswith(header) for header in ignore_titles)
+        ]
+        all_articles.extend(articles)
 
-    # Wait for the page to load (if necessary)
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    driver.quit()
-    text = [line for line in soup.text.split("\n") if "Entire Issue" in line][0]
-    pdf_url = soup.find("a", string=text)["href"]
-    parsed_url = urlparse(SOURCES["congressional record"])
-    pdf_url = urljoin(f"{parsed_url.scheme}://{parsed_url.netloc}", pdf_url)
+    for article in tqdm(all_articles):
+        text = BeautifulSoup(fetch_url(article["url"])).text
+        sentences = [
+            re.sub(" +", " ", sentence)
+            for paragraph in text.split("\n\n")
+            if paragraph.strip()
+            for sentence in split_sentences(
+                paragraph.replace("\n", " ").strip(), min_words=1
+            )
+        ]
+        article["text"] = text
+        article["sentences"] = sentences
 
-    # Download the PDF to a temporary file
-    response = requests.get(pdf_url)
-    response.raise_for_status()
-
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_pdf:
-        temp_pdf.write(response.content)
-        temp_pdf_path = temp_pdf.name
-
-    # Process the PDF (if needed)
-    text, _ = pdf_to_text(temp_pdf_path)
-    sentences = split_sentences(text)
-    return sentences
+    return all_articles
